f0841f3633077b3e2920ec3d8876b012ea22f9fd
Johnathan Fercher <johnathanfercher22@gmail.com>
2017-11-29 22:19:38 -0200

    Compiling in linux using cmake and make.

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 GLUT Object Oriented Framework
-Copyright 2010 © Paul Solt
+Copyright 2010 ï¿½ Paul Solt
 Update Date: 8/22/10
 Release 1.0.0
 -------------------------------
 
 Overview:
 ---------
-The GLUT Object Oriented Framework is a set of classes that wrap around the GLUT C-style function calls to create an animation framework. It provides a simple object oriented wrapper that has virtual methods to override default behavior. Four lines of code within a main function can be used to get a graphics window that draws an animated teapot. 
+The GLUT Object Oriented Framework is a set of classes that wrap around the GLUT C-style function calls to create an animation framework. It provides a simple object oriented wrapper that has virtual methods to override default behavior. Four lines of code within a main function can be used to get a graphics window that draws an animated teapot.
 
 	Teapot Demo:
 	------------
@@ -18,7 +18,7 @@ The GLUT Object Oriented Framework is a set of classes that wrap around the GLUT
 		framework.startFramework(argc, argv);
 	    return 0;
 	}
-	
+
 	Subclass Example:
    	-----------------
    	MySubClassOfGlutFramework framework;
@@ -31,6 +31,10 @@ Cross-platform Support:
 2. Visual Studio 2010 project for Windows .NET 2.0
 	a. GLUT must be installed and setup http://www.xmission.com/~nate/glut.html
 	b. Setup GLUT via: http://www.cs.uiowa.edu/~cwyman/classes/common/howto/winGLUT.html
+3. Project that builds for Linux
+ 	a. Must install cmake
+	b. Create a folder, can have any name
+	c. Go inside the folder, **run cmake ..**. After cmake created the Makefile, run **make**
 
 Animation Framework:
 --------------------
--- src/GlutFramework.h
+++ src/GlutFramework.h
@@ -1,7 +1,7 @@
 /*
  * The MIT License
  *
- * Copyright (c) 2010 Paul Solt, PaulSolt@gmail.com 
+ * Copyright (c) 2010 Paul Solt, PaulSolt@gmail.com
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -9,10 +9,10 @@
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -34,12 +34,15 @@
 
 // **Note:** Include GLUT after the standard c++ libraries to prevent linker errors
 
-#ifdef WIN32
-	#include <windows.h> 
-	#include <GL/glut.h> 
-#else
-	#include <GLUT/glut.h>
+#ifdef _WIN32
+	#include <windows.h>
+	#include <GL/glut.h>
+#elif __APPLE__
+  #include <GLUT/glut.h>
+#elif __linux
+	#include <GL/glut.h>
 #endif
+
 
 
 #include "Keyboard.h"
@@ -48,7 +51,7 @@
 
 /** GlutFramework.h
  *
- * The GlutFramework defines a super class that can be used to create a graphics 
+ * The GlutFramework defines a super class that can be used to create a graphics
  * application that requires an animation loop. The frame rate is set by default
  * to 60 frames per second.
  *
@@ -73,29 +76,29 @@
  * @author Paul Solt 8-22-10
  */
 namespace glutFramework {
-		
+
 	class GlutFramework {
-		
+
 	private:
 		double frameTimeElapsed;
-		
+
 	protected:
 		Keyboard keyStates;
 		PerformanceTimer frameRateTimer;
 		PerformanceTimer displayTimer;
-		
+
 		std::string title;
-		double elapsedTimeInSeconds;  
+		double elapsedTimeInSeconds;
 		static GlutFramework *instance;
-		
-		
+
+
 		Vector<float> eyeVector;
 		Vector<float> centerVector;
 		Vector<float> upVector;
-		
+
 		float position;
 		float direction;
-		
+
 	public:
 		// Constants
 		const static int FPS = 60;			// Frames per second
@@ -103,134 +106,134 @@ namespace glutFramework {
 		const static int WINDOW_HEIGHT = 480;
 		const static int WINDOW_X_POSITION = 100;
 		const static int WINDOW_Y_POSITION = 100;
-		
+
 		const static double FRAME_TIME;		// Frame time is in milliseconds calculated using FPS
-		
+
 	public:
 		GlutFramework();
 		virtual ~GlutFramework();
-		
+
 		/** startFramework will initialize framework and start the Glut run loop. It
-		 * must be called after the GlutFramework class is created to start the application. 
+		 * must be called after the GlutFramework class is created to start the application.
 		 */
 		void startFramework(int argc, char *argv[]);
-		
+
 		// ****************************
-		// ** Subclass and Implement ** 
+		// ** Subclass and Implement **
 		// ****************************
-		
+
 		/** Any loading logic can be down in this method when the application starts. */
 		virtual void load();
-		
+
 		/** The display function is called at a specified frames-per-second (FPS). Any
 		 animation drawing code can be run in the display method.
 		 @param dTime - the change in time (seconds)
 		 */
 		virtual void display(float dTime);
-		
+
 		/** Called when the window dimensions change.
 		 @param width - the width of the window in pixels
 		 @param height - the height of the window in pixels
 		 */
 		virtual void reshape(int width, int height);
-		
-		/** Called when the mouse buttons are pressed. 
+
+		/** Called when the mouse buttons are pressed.
 		 @param button - the mouse buttons
 		 @param state - the state of the buttons
 		 @param x - the x coordinate
 		 @param y - the y coordinate
 		 */
 		virtual void mouseButtonPress(int button, int state, int x, int y);
-		
+
 		/** Called when the mouse moves on the screen.
 		 @param x - the x coordinate
 		 @param y - the y coordinate
 		 */
 		virtual void mouseMove(int x, int y);
-		
+
 		/** The keyboard function is called when a standard key is pressed down.
 		 @param key - the key press
 		 @param x - the x coordinate of the mouse
 		 @param y - the y coordinate of the mouse
 		 */
 		virtual void keyboardDown( unsigned char key, int x, int y );
-		
+
 		/** The keyboard function is called when a standard key is "unpressed".
 		 @param key - the key press
 		 @param x - the x coordinate of the mouse
 		 @param y - the y coordinate of the mouse
 		 */
 		virtual void keyboardUp( unsigned char key, int x, int y );
-		
+
 		/** The keyboard function is called when a special key is pressed down.
-		 (F1 keys, Home, Inser, Delete, Page Up/Down, End, arrow keys) 
+		 (F1 keys, Home, Inser, Delete, Page Up/Down, End, arrow keys)
 		 http://www.opengl.org/resources/libraries/glut/spec3/node54.html
 		 @param key - the key press
 		 @param x - the x coordinate of the mouse
 		 @param y - the y coordinate of the mouse
 		 */
 		virtual void specialKeyboardDown( int key, int x, int y );
-		
+
 		/** The keyboard function is called when a special key is "unpressed.
-		 (F1 keys, Home, Inser, Delete, Page Up/Down, End, arrow keys) 
+		 (F1 keys, Home, Inser, Delete, Page Up/Down, End, arrow keys)
 		 @param key - the key press
 		 @param x - the x coordinate of the mouse
 		 @param y - the y coordinate of the mouse
 		 */
-		virtual void specialKeyboardUp( int key, int x, int y ); 
-		
+		virtual void specialKeyboardUp( int key, int x, int y );
+
 		/** Sets the intitial matricies that are used to setup OpenGL. */
 		void setDisplayMatricies();
-		
+
 		/** Sets up basic lighting */
 		void setupLights();
-		
-		/** A helper function that allows the camera position and orientation to be changed. 
-		 @param eyeX, eyeY, eyeZ - the vector describing the "eye" or camera position 
+
+		/** A helper function that allows the camera position and orientation to be changed.
+		 @param eyeX, eyeY, eyeZ - the vector describing the "eye" or camera position
 		 @param centerX, centerY, centerZ - the vector describing the "object" that the eye is looking at
-		 @param upX, upY, upZ - the vector describing orientation. Normally (0,1,0) 
+		 @param upX, upY, upZ - the vector describing orientation. Normally (0,1,0)
 		 */
-		void setLookAt(float eyeX, float eyeY, float eyeZ, 
+		void setLookAt(float eyeX, float eyeY, float eyeZ,
 					   float centerX, float centerY, float centerZ,
 					   float upX, float upY, float upZ);
-		
-		/** The position of the "eye" or camera in 3D space 
+
+		/** The position of the "eye" or camera in 3D space
 		 @return a 3D vector
 		 */
 		Vector<float> getEyeVector() const;
-		
-		/** The position that the "eye" or camera is looking at in 3D space 
+
+		/** The position that the "eye" or camera is looking at in 3D space
 		 @return a 3D vector
 		 */
 		Vector<float> getCenterVector() const;
-		
-		/** The up vector, used to determine orientation. Normally set to (0,1,0) 
+
+		/** The up vector, used to determine orientation. Normally set to (0,1,0)
 		 @return a 3D vector
 		 */
 		Vector<float> getUpVector() const;
-		
-		/** Sets the title of the window to a specific string. Invoke before startFramework() 
-		 @param title - the name of the window 
+
+		/** Sets the title of the window to a specific string. Invoke before startFramework()
+		 @param title - the name of the window
 		 */
 		void setTitle(std::string theTitle);
-		
+
 		/** Initializes GLUT and registers the callback functions. */
 		void init();
-		
+
 		/** The instance will be set when the framework is started, this step is necessary
-		 * to initialize the static instance that is used for the object oriented wrapper calls 
+		 * to initialize the static instance that is used for the object oriented wrapper calls
 		 */
-		void setInstance(); 
-		
+		void setInstance();
+
 		/** The run method is called by GLUT and contains the logic to set the framerate of the application. */
 		void run();
-		
+
 		/** The displayFramework() function sets up initial GLUT state and calculates the
 		 change in time between each frame. It calls the display(float) function which can
 		 be subclassed.
 		 */
 		void displayFramework();
-		
+
 		// ** Static functions which are passed to GLUT function callbacks **
 		// http://www.parashift.com/c++-faq-lite/pointers-to-members.html#faq-33.1
 		static void displayWrapper();
@@ -243,6 +246,6 @@ namespace glutFramework {
 		static void specialKeyboardDownWrapper(int key, int x, int y);
 		static void specialKeyboardUpWrapper(int key, int x, int y);
 	};
-	
-} // namespace 
+
+} // namespace
 #endif
